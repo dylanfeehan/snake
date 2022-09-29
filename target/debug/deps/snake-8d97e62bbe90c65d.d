8d024378ede535af115fea43951d99147e4bc3ec
Dylan Feehan <dylan@linux.myfiosgateway.com>
2022-09-29 16:22:41 -0400

    initial commit

---
+++ target/debug/deps/snake-8d97e62bbe90c65d.d
@@ -0,0 +1,5 @@
+/home/dylan/cs/projects/snake/target/debug/deps/snake-8d97e62bbe90c65d: src/main.rs
+
+/home/dylan/cs/projects/snake/target/debug/deps/snake-8d97e62bbe90c65d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/snake.d
@@ -0,0 +1,1 @@
+/home/dylan/cs/projects/snake/target/debug/snake: /home/dylan/cs/projects/snake/src/main.rs
